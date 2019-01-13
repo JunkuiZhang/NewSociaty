40daa5957e52612869d170c636ace9bd38402884
Junkui Zhang <364772080@qq.com>
2019-01-13 21:44:15 +0800

    animation init

--- Animation/Animation.py
+++ Animation/Animation.py
@@ -5,8 +5,35 @@ from pygame.locals import *
 
 class Animation:
 
-    def __init__(self, resolution, dimension, entities):
-        pass
+    def __init__(self, resolution, dimension, world, entities):
+        self.__resolution = resolution
+        self.__dimension = dimension
+        self.__world = world
+        self.__entities = entities
 
-    def run(self):
+    @property
+    def resolution(self):
+        return self.__resolution
+
+    @resolution.setter
+    def resolution(self, num):
+        self.__resolution = num
+
+    @property
+    def dimension(self):
+        return self.__dimension
+
+    @dimension.setter
+    def dimension(self, num):
+        self.__dimension = num
+
+    @property
+    def world(self):
+        return self.__world
+
+    @property
+    def entities(self):
+        return self.__entities
+
+    def playing(self):
         pass
