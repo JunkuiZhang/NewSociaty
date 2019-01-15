78429a8543440e9978530cf0bfcc1c5bf590a835
Junkui Zhang <364772080@qq.com>
2019-01-15 20:33:03 +0800

    animation property updated

--- Animation/Animation.py
+++ Animation/Animation.py
@@ -6,27 +6,12 @@ import MathBehind.FindMaxValue
 
 class Animation:
 
-    def __init__(self, resolution, dimension, world, entities):
-        self.__resolution = resolution
-        self.__dimension = dimension
+    def __init__(self, world, entities):
+        # World object
         self.__world = world
         self.__entities = entities
-
-    @property
-    def resolution(self):
-        return self.__resolution
-
-    @resolution.setter
-    def resolution(self, num):
-        self.__resolution = num
-
-    @property
-    def dimension(self):
-        return self.__dimension
-
-    @dimension.setter
-    def dimension(self, num):
-        self.__dimension = num
+        self.__resolution = self.world.resolution
+        self.__dimension = self.world.dimension
 
     @property
     def world(self):
@@ -36,15 +21,22 @@ class Animation:
     def entities(self):
         return self.__entities
 
+    @property
+    def resolution(self):
+        return self.__resolution
+
+    @property
+    def dimension(self):
+        return self.__dimension
+
     def playing(self):
         pixes_per_unit = round(self.resolution/self.dimension, 0)
         pygame.init()
         screen = pygame.display.set_mode((self.resolution, self.resolution))
         pygame.display.set_caption('Society')
-        max_product = MathBehind.FindMaxValue.MaxFind(self.world)
+        max_product = MathBehind.FindMaxValue.MaxFind(self.world.world_grid.matrix)
 
         while True:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     exit()
-                
