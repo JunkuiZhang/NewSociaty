5da18a699221bf6c9bc3acbb128a0983b2e8edbe
Junkui Zhang <364772080@qq.com>
2019-01-16 22:14:45 +0800

    test

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import Entities.Entities
 import Animation.Animation
 
 if __name__ == '__main__':
-    w = World.World.World(16, random_seed=1)
-    pop = Entities.Entities.EntitiesPopulation(w, 5)
+    w = World.World.World(36, random_seed=1, inflation=0)
+    pop = Entities.Entities.EntitiesPopulation(w, 200)
     pop.population_init()
     w.population_position_insert(pop)
     a = Animation.Animation.Animation(w, pop)
